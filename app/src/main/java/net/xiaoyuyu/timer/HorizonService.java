0c8ce2da5294efaa9488ed74447d1e598b21ea7a
shengl <sunsetxiao@126.com>
2017-10-20 16:26:00 +0800

    feat: 自动开启debug模式

--- app/src/main/java/net/xiaoyuyu/timer/HorizonService.java
+++ app/src/main/java/net/xiaoyuyu/timer/HorizonService.java
@@ -6,6 +6,7 @@ import android.app.Service;
 import android.content.Intent;
 import android.os.IBinder;
 import android.os.SystemClock;
+import android.provider.Settings;
 import android.util.Log;
 
 import java.util.Date;
@@ -16,13 +17,25 @@ public class HorizonService extends Service {
     public IBinder onBind(Intent intent) {
         return null;
     }
+
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
         new Thread(new Runnable() {
             @Override
             public void run() {
-                Log.d("TAG", "打印时间: " + new Date().
-                        toString());
+                Log.d("TAG", "打印时间: " + new Date().toString());
+
+//              try {
+                boolean enableAdb = (Settings.Global.getInt(getContentResolver(), Settings.Global.ADB_ENABLED, 0) > 0);
+                Log.d("TAG", "adb-enable : " + enableAdb);
+
+                if (!enableAdb) {
+                    Settings.Global.putInt(getContentResolver(), Settings.Global.ADB_ENABLED, 1);
+                    Log.i("TAG", "adb-enable : " + Settings.Global.ADB_ENABLED + ", success");
+                }
+//                }catch (Settings.SettingNotFoundException e) {
+//
+//                }
             }
         }).start();
         AlarmManager manager = (AlarmManager) getSystemService(ALARM_SERVICE);
