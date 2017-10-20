a8c835bd81ecf138dc8fe1a8d0a8a69858c45c07
shengl <sunsetxiao@126.com>
2017-10-20 17:26:19 +0800

    feat: 开机自动启动

--- app/src/main/java/net/xiaoyuyu/timer/BootBroadcastReceiver.java
+++ app/src/main/java/net/xiaoyuyu/timer/BootBroadcastReceiver.java
@@ -3,6 +3,7 @@ package net.xiaoyuyu.timer;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.util.Log;
 
 public class BootBroadcastReceiver extends BroadcastReceiver {
 
@@ -11,11 +12,10 @@ public class BootBroadcastReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
         if (intent.getAction().equals(ACTION)) {
+            Log.i("TAG", "开机了~~");
             Intent mainActivityIntent = new Intent(context, MainActivity.class);  // 要启动的Activity
             mainActivityIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
             context.startActivity(mainActivityIntent);
-
-
         }
     }
 }
